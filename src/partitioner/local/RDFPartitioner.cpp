4352bee872669deefbbfafc3a699bc99623ba1b0
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2019-01-26 23:24:58 +0530

    Partially implemented RDFPartitioner

--- src/partitioner/local/RDFPartitioner.cpp
+++ src/partitioner/local/RDFPartitioner.cpp
@@ -2,3 +2,41 @@
 // Created by shalika-madhushanki on 1/25/19.
 //
 
+#include "RDFPartitioner.h"
+
+
+void RDFPartitioner::convert(string graphName, string graphID, string:inputFilePath, string outputFilePath, int nParts, bool isDistributedCentralPartitions, int nThreads, int nPlaces) {
+    convertWithoutDistribution(graphName, graphID, inputFilePath, outputFilePath, nParts, isDistributedCentralPartitions, nThreads, nPlaces);
+    distributeEdges();
+}
+
+void MetisPartitioner:: convertWithoutDistribution(string graphName, string graphID, string inputFilePath, string outputFilePath, int nParts, bool isDistributedCentralPartitions, int nThreads, int nPlaces){
+    this.outputFilePath = outputFilePath;
+    this.nParts = nParts;
+    this.graphName = graphName;
+    this.isDistributedCentralPartitions = isDistributedCentralPartitions;
+    this.graphID = graphID;
+    this.nThreads = nThreads;
+    this.nPlaces = nPlaces;
+
+    //The following number of Treemap instances is kind of tricky, but it was decided to use nThreads number of TreeMaps to improve the data loading performance.
+    /*    graphStorage = new Rail[TreeMap](nThreads);
+
+    for(var i:int = 0n; i < nThreads; i++){
+    graphStorage(i) = new TreeMap();
+    }
+
+    loadDataSet(inputFilePath);
+    constructMetisFormat(-1n);
+    partitionWithMetis(nParts);
+     */
+
+}
+
+void distributeEdges(){
+    //method implementation
+}
+
+void loadDataSet(){
+
+}
--- src/partitioner/local/RDFPartitioner.h
+++ src/partitioner/local/RDFPartitioner.h
@@ -6,7 +6,71 @@
 #define JASMINEGRAPH_RDFPARTITIONER_H
 
 
-class RDFPartitioner {
+
+
+#include <string>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <unordered_set>
+#include <set>
+#include "metis.h"
+#include "../../metadb/SQLiteDBInterface.h"
+#include "../../util/Utils.h"
+#include <cstddef>
+#include <algorithm>
+
+using std::string;
+
+
+class RDFPartitioner  {
+public:
+    void convert(string graphName, string graphID, string:inputFilePath, string outputFilePath, int nParts, bool isDistributedCentralPartitions, int nThreads, int nPlaces);
+    void convertWithoutDistribution(string graphName, string graphID, string inputFilePath, string outputFilePath, int nParts, bool isDistributedCentralPartitions, int nThreads, int nPlaces);
+    void loadDataSet(string inputFilePath, string outputFilePath);
+    void distributeEdges();
+    //void partitionGraph();
+//    void constructMetisFormat();
+//    void partitioneWithGPMetis();
+    //MetisPartitioner(SQLiteDBInterface*);
+private:
+
+    string outputFilePath;
+    int nParts;
+    string graphName;
+    string graphID;
+    int nThreads;
+    int nPlaces;
+
+    string inputFilePath;
+
+//private:
+//    idx_t edgeCount = 0;
+//    idx_t largestVertex = 0;
+//    idx_t vertexCount = 0;
+//    //TODO:Need to remove this hardcoded value
+//    idx_t nParts = 4;
+//    string outputFilePath;
+//    bool zeroflag = false;
+//    SQLiteDBInterface sqlite;
+
+
+//private var nodes:HashMap[String,Long] = new HashMap[String,Long]();
+//private var nodesTemp:HashMap[Long,String] = new HashMap[Long,String]();
+//private var predicates:HashMap[String,Long] = new HashMap[String,Long]();
+//private var predicatesTemp:HashMap[Long,String] = new HashMap[Long,String]();
+//private var relationsMap:HashMap[Long,HashMap[Long,ArrayList[String]]] = new HashMap[Long,HashMap[Long,ArrayList[String]]]();
+//private var attributeMap:HashMap[Long,HashMap[Long,ArrayList[String]]] = new HashMap[Long,HashMap[Long,ArrayList[String]]]();
+
+    std::map<string, long> nodes;
+    std::map<long, string> nodesTemp;
+    std::map<string, long> predicates;
+    std::map<long, string> predicatesTemp;
+    std::map<long, std::vector<string>> relationsMap;
+    std::map<long, std::vector<string>> attributeMap;
 
 };
 
