163e0344e79f339f47ec2aa69d89c4e63d51eba5
DinikaSen <dinikasen@gmail.com>
2019-01-27 19:16:10 +0530

    refine code

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 #Below the argument 1 says Jasminegraph needs to be instantiated in the Master mode
-#/home/$USER/git/jasminegraph/cmake-build-debug/JasmineGraph 1
-/home/$USER/FYP/jasminegraph/cmake-build-debug/JasmineGraph 1
+/home/$USER/git/jasminegraph/cmake-build-debug/JasmineGraph 1
