c32513522d222e8f28dfe836a3918a02e7ffbf7f
Miyuru Dayarathna <miyurud@yahoo.co.uk>
2019-02-13 22:33:27 +0530

    Improving the build process using the system's cmake installation

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-mkdir -p /home/$USER/git/jasminegraph/cmake-build-debug
-/home/$USER/software/clion-2018.1.3/bin/cmake/bin/cmake --build /home/$USER/git/jasminegraph/cmake-build-debug --target JasmineGraph -- -j 2
+cmake --build /home/$USER/git/jasminegraph/cmake-build-debug --target JasmineGraph -- -j 2
