50331a793fbb61f612d72c975dfe1ecae8d868b5
AnuradhaSK <anuradha199528@gmail.com>
2019-06-18 12:54:59 +0530

    Adjusted train model saving path

--- GraphSAGE/graphsage/utils.py
+++ GraphSAGE/graphsage/utils.py
@@ -6,6 +6,7 @@ import json
 import sys
 import os
 
+import tensorflow as tf
 import networkx as nx
 from networkx.readwrite import json_graph
 from collections import defaultdict
@@ -18,19 +19,24 @@ assert (major <= 1) and (minor <= 11), "networkx major version > 1.11"
 WALK_LEN = 5
 N_WALKS = 50
 
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
 
 def preprocess_data(prefix, worker):
     np.random.seed(1)
     random.seed(1)
     # G = nx.read_edgelist(prefix + '_' + worker, nodetype=int)
     G_local = nx.read_edgelist(prefix + '_' + worker, nodetype=int)
-    G_central = nx.read_edgelist(prefix + '_centralstore_' + worker, nodetype = int)
-    G = nx.compose(G_local,G_central)
+    G_central = nx.read_edgelist(prefix + '_centralstore_' + worker, nodetype=int)
+    G = nx.compose(G_local, G_central)
     nodes_key_list = list(G.nodes())
-
+    save_dir = FLAGS.base_log_dir + "/jasminegraph-local_trained_model_store/"
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
     # print(nodes_key_list)
     # shuffle data
-    nodes_key_list= np.random.permutation(nodes_key_list)
+    nodes_key_list = np.random.permutation(nodes_key_list)
     # random.shuffle(nodes_key_list)
     test_val_frac = 0.2
     for i, n in enumerate(nodes_key_list):
@@ -47,7 +53,7 @@ def preprocess_data(prefix, worker):
     data = json_graph.node_link_data(G)
     print(len(data['nodes']))
 
-    with open(prefix + '_' + worker + "-G.json", mode="w") as f:
+    with open(save_dir + str(FLAGS.graph_id) + '_' + worker + "-G.json", mode="w") as f:
         f.write(json.dumps(data))
 
     # TODO Try to get number of attri. from db
@@ -56,11 +62,11 @@ def preprocess_data(prefix, worker):
 
     num_nodes = len(nodes_key_list)
     num_feats = len(first_line) - 2
-    print(num_feats)
+    # print(num_feats)
 
     feat_data = np.zeros((num_nodes, num_feats))
     labels = np.empty((num_nodes, 1), dtype=np.int64)
-    print(labels.shape)
+    # print(labels.shape)
     node_map = {}
     label_map = {}
 
@@ -75,7 +81,7 @@ def preprocess_data(prefix, worker):
             if not info[-1] in label_map:
                 label_map[info[-1]] = len(label_map)
             labels[i] = label_map[info[-1]]
-        next_index = i+1
+        next_index = i + 1
 
     with open(prefix + "_centralstore_attributes_" + worker) as fp:
         for i, line in enumerate(fp):
@@ -88,10 +94,9 @@ def preprocess_data(prefix, worker):
                 if not info[-1] in label_map:
                     label_map[info[-1]] = len(label_map)
                 labels[next_index] = label_map[info[-1]]
-                next_index +=1
+                next_index += 1
 
-
-    with open(prefix + "_" + worker + "-id_map.json", mode="w") as f:
+    with open(save_dir + str(FLAGS.graph_id) + "_" + worker + "-id_map.json", mode="w") as f:
         f.write(json.dumps(node_map))
 
     labels_reshape = labels.flatten()
@@ -110,15 +115,16 @@ def preprocess_data(prefix, worker):
 
     # print(class_map)
     # print(list(class_map.values())[2])
-    with open(prefix + "_" + worker + "-class_map.json", mode="w") as f:
+    with open(save_dir + str(FLAGS.graph_id) + "_" + worker + "-class_map.json", mode="w") as f:
         f.write(json.dumps(class_map, default=int))
 
-    np.save(prefix + "_" + worker + '-feats.npy', feat_data)
+    np.save(save_dir + str(FLAGS.graph_id) + "_" + worker + '-feats.npy', feat_data)
     return load_data(prefix, worker)
 
 
 def load_data(prefix, worker, normalize=True, load_walks=False):
-    G_data = json.load(open(prefix +"_"+worker +"-G.json"))
+    save_dir = FLAGS.base_log_dir + "/jasminegraph-local_trained_model_store/"
+    G_data = json.load(open(save_dir + str(FLAGS.graph_id) + "_" + worker + "-G.json"))
     G = json_graph.node_link_graph(G_data)
     # print("G.nodes()[0] -----")
     # print(G.nodes()[0])
@@ -127,16 +133,16 @@ def load_data(prefix, worker, normalize=True, load_walks=False):
     else:
         conversion = lambda n: n
 
-    if os.path.exists(prefix + "_" + worker + "-feats.npy"):
-        feats = np.load(prefix + "_" + worker + "-feats.npy")
+    if os.path.exists(save_dir + str(FLAGS.graph_id) + "_" + worker + "-feats.npy"):
+        feats = np.load(save_dir + str(FLAGS.graph_id) + "_" + worker + "-feats.npy")
     else:
         print("No features present.. Only identity features will be used.")
         feats = None
-    id_map = json.load(open(prefix + "_" + worker + "-id_map.json"))
+    id_map = json.load(open(save_dir + str(FLAGS.graph_id) + "_" + worker + "-id_map.json"))
     id_map = {conversion(k): int(v) for k, v in id_map.items()}
     # id_map doesn't have an order
     walks = []
-    class_map = json.load(open(prefix + "_" + worker + "-class_map.json"))
+    class_map = json.load(open(save_dir + str(FLAGS.graph_id) + "_" + worker + "-class_map.json"))
     if isinstance(list(class_map.values())[0], list):
         lab_conversion = lambda n: n
     else:
@@ -177,7 +183,7 @@ def load_data(prefix, worker, normalize=True, load_walks=False):
         # print(feats.shape)
 
     if load_walks:
-        with open(prefix + "_" + worker + "-walks.txt") as fp:
+        with open(save_dir + str(FLAGS.graph_id) + "_" + worker + "-walks.txt") as fp:
             for line in fp:
                 walks.append(map(conversion, line.split()))
 
