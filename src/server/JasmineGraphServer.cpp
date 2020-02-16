3aeb782031e5e3830cf9d3d0e0f328eff40700d0
chinthaka <chinthakarukshan@gmail.com>
2020-02-16 19:26:37 +0530

    Adding port mapping to worker docker

--- src/server/JasmineGraphServer.cpp
+++ src/server/JasmineGraphServer.cpp
@@ -204,7 +204,14 @@ void JasmineGraphServer::startRemoteWorkers(std::vector<int> workerPortsVector,
                         "ssh -p 22 " + host + " " + executableFile + " 2 "+host+" " + std::to_string(workerPortsVector.at(i)) +
                         " " + std::to_string(workerDataPortsVector.at(i));
             }
-            serverStartScript = "docker -H ssh://root@" + host + " run jasmine_graph:latest --MODE 2 --HOST_NAME " + host + " --MASTERIP " + masterHost + " --SERVER_PORT " + std::to_string(workerPortsVector.at(i)) + " --SERVER_DATA_PORT " + std::to_string(workerDataPortsVector.at(i));
+            serverStartScript = "docker -H ssh://root@" + host + " run jasmine_graph:latest -p " +
+                                std::to_string(workerPortsVector.at(i)) + ":" +
+                                std::to_string(workerPortsVector.at(i)) + " -p " +
+                                std::to_string(workerDataPortsVector.at(i)) + ":" +
+                                std::to_string(workerDataPortsVector.at(i)) + "--MODE 2 --HOST_NAME " + host +
+                                " --MASTERIP " + masterHost + " --SERVER_PORT " +
+                                std::to_string(workerPortsVector.at(i)) + " --SERVER_DATA_PORT " +
+                                std::to_string(workerDataPortsVector.at(i));
             server_logger.log(serverStartScript, "info");
             popen(serverStartScript.c_str(),"r");
         }
