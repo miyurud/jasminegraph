9c010d728c45e30e9b9d50eb7026a8647fb21a55
DamithaSenevirathne <damithasenevirathne.16@cse.mrt.ac.lk>
2020-10-11 21:01:35 +0530

    Add functionality to train multiple partitions

--- src/ml/federated/JasmineGraphFederatedInstance.cpp
+++ src/ml/federated/JasmineGraphFederatedInstance.cpp
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <string>
 #include <map>
+#include <iostream>
 #include "JasmineGraphFederatedInstance.h"
 #include "../../server/JasmineGraphInstanceProtocol.h"
 #include "../../util/Utils.h"
 #include "../trainer/JasmineGraphTrainingSchedular.h"
 #include "../../server/JasmineGraphServer.h"
-#include <iostream>
 #include "../../util/logger/Logger.h"
 #include "../../util/Utils.h"
 
@@ -15,7 +15,7 @@
 Logger fed_trainer_log;
 
 void JasmineGraphFederatedInstance::initiateFiles(std::string graphID, std::string trainingArgs) {
-    std::cout << "Initiating training.." << std::endl;
+
     int count = 0;
     JasmineGraphTrainingSchedular *schedular = new JasmineGraphTrainingSchedular();
     map<string, map<int, int>> scheduleForAllHosts = schedular->schedulePartitionTraining(graphID);
@@ -60,27 +60,111 @@ void JasmineGraphFederatedInstance::initiateFiles(std::string graphID, std::stri
     }
 }
 
-void JasmineGraphFederatedInstance::initiateCommunication(std::string graphID, std::string trainingArgs,SQLiteDBInterface sqlite) {
+void JasmineGraphFederatedInstance::initiateCommunication(std::string graphID, std::string trainingArgs, SQLiteDBInterface sqlite) {
+
+    int count = 0;
+    JasmineGraphTrainingSchedular *schedular = new JasmineGraphTrainingSchedular();
+    map<string, map<int, int>> scheduleForAllHosts = schedular->schedulePartitionTraining(graphID);
+    JasmineGraphServer *jasmineServer = new JasmineGraphServer();
+    std::map<std::string, JasmineGraphServer::workerPartitions> graphPartitionedHosts = jasmineServer->getGraphPartitionedHosts(
+            graphID);
+    int partition_count = 0;
+    std::map<std::string, JasmineGraphServer::workerPartitions>::iterator mapIterator;
+    for (mapIterator = graphPartitionedHosts.begin(); mapIterator != graphPartitionedHosts.end(); mapIterator++) {
+        JasmineGraphServer::workerPartitions workerPartition = mapIterator->second;
+        std::vector<std::string> partitions = workerPartition.partitionID;
+        std::vector<std::string>::iterator it;
+        for (it = partitions.begin(); it < partitions.end(); it++) {
+            partition_count++;
+        }
+    }
+    std::cout << partition_count << std::endl;
+    std::thread *workerThreads = new std::thread[partition_count+1];
 
     Utils utils;
-    std::vector<Utils::worker> workerArr = utils.getWorkerList(sqlite);
-    std::thread *workerThreads = new std::thread[3];
+    trainingArgs = trainingArgs;
 
-    int limit = workerArr.size();
-    int thread_id = 0;
-    int poolSize = workerArr.size()+1;
+    int fl_clients = stoi(utils.getJasmineGraphProperty("org.jasminegraph.fl_clients"));
 
-    workerThreads[0] = std::thread(initiateServer,"localhost", 7780, 7781,trainingArgs,2, "1");
-    sleep(10);        
-    workerThreads[1] = std::thread(initiateClient,"localhost", 7780, 7781,trainingArgs + ' '+ '0',2, "1");    
-    sleep(5);
-    workerThreads[2] = std::thread(initiateClient,"localhost", 7782, 7783,trainingArgs + ' '+ '0',2, "1");
-    sleep(5); 
-        
-    for (int threadCount = 0; threadCount < poolSize; threadCount++) {
-        workerThreads[threadCount].join();
-        std::cout << "Fed Thread " << threadCount << " joined" << std::endl;
+    workerThreads[0] = std::thread(initiateServer,"localhost", 7780, 7781,trainingArgs,fl_clients, "1");
+
+    count = 1;
+
+    std::map<std::string, JasmineGraphServer::workerPartitions>::iterator j;
+    for (j = graphPartitionedHosts.begin(); j != graphPartitionedHosts.end(); j++) {
+        JasmineGraphServer::workerPartitions workerPartition = j->second;
+        std::vector<std::string> partitions = workerPartition.partitionID;
+        string partitionCount = std::to_string(partitions.size());
+        std::vector<std::string>::iterator k;
+        map<int, int> scheduleOfHost = scheduleForAllHosts[j->first];
+        for (k = partitions.begin(); k != partitions.end(); k++) {
+            int iterationOfPart = scheduleOfHost[stoi(*k)];
+            workerThreads[count] = std::thread(initiateClient,"localhost", workerPartition.port, workerPartition.dataPort,trainingArgs + " " + *k, fl_clients, *k);
+            count++;
+        }
     }
+
+    for (int threadCount = 0; threadCount < count; threadCount++) {
+        workerThreads[threadCount].join();
+        std::cout << "Thread " << threadCount << " joined" << std::endl;
+    }
+
+    std::cout << "Federated learning commands sent" <<std::endl;
+
+}
+
+
+void JasmineGraphFederatedInstance::initiateMerge(std::string graphID, std::string trainingArgs,SQLiteDBInterface sqlite) {
+
+
+    int count = 0;
+    JasmineGraphTrainingSchedular *schedular = new JasmineGraphTrainingSchedular();
+    map<string, map<int, int>> scheduleForAllHosts = schedular->schedulePartitionTraining(graphID);
+    JasmineGraphServer *jasmineServer = new JasmineGraphServer();
+    std::map<std::string, JasmineGraphServer::workerPartitions> graphPartitionedHosts = jasmineServer->getGraphPartitionedHosts(
+            graphID);
+    int partition_count = 0;
+    std::map<std::string, JasmineGraphServer::workerPartitions>::iterator mapIterator;
+    for (mapIterator = graphPartitionedHosts.begin(); mapIterator != graphPartitionedHosts.end(); mapIterator++) {
+        JasmineGraphServer::workerPartitions workerPartition = mapIterator->second;
+        std::vector<std::string> partitions = workerPartition.partitionID;
+        std::vector<std::string>::iterator it;
+        for (it = partitions.begin(); it < partitions.end(); it++) {
+            partition_count++;
+        }
+    }
+    std::cout << partition_count << std::endl;
+    std::thread *workerThreads = new std::thread[partition_count+1];
+
+    Utils utils;
+    trainingArgs = trainingArgs;
+
+    int fl_clients = stoi(utils.getJasmineGraphProperty("org.jasminegraph.fl_clients"));
+
+
+    std::map<std::string, JasmineGraphServer::workerPartitions>::iterator j;
+    for (j = graphPartitionedHosts.begin(); j != graphPartitionedHosts.end(); j++) {
+        JasmineGraphServer::workerPartitions workerPartition = j->second;
+        std::vector<std::string> partitions = workerPartition.partitionID;
+        string partitionCount = std::to_string(partitions.size());
+        std::vector<std::string>::iterator k;
+        map<int, int> scheduleOfHost = scheduleForAllHosts[j->first];
+        for (k = partitions.begin(); k != partitions.end(); k++) {
+            int iterationOfPart = scheduleOfHost[stoi(*k)];
+
+            workerThreads[count] = std::thread(mergeFiles,"localhost", workerPartition.port, workerPartition.dataPort,trainingArgs + " " + *k, fl_clients, *k);
+            count++;
+        }
+    }
+
+    std::cout << count <<std::endl;
+
+    for (int threadCount = 0; threadCount < count; threadCount++) {
+        workerThreads[threadCount].join();
+        std::cout << "Thread " << threadCount << " joined" << std::endl;
+    }
+
+    std::cout << "merge commands sent" << std::endl;
 }
 
 
@@ -124,7 +208,7 @@ bool JasmineGraphFederatedInstance::initiateTrain(std::string host, int port, in
 
     bzero(data, FED_DATA_LENGTH);
     write(sockfd, JasmineGraphInstanceProtocol::HANDSHAKE.c_str(), JasmineGraphInstanceProtocol::HANDSHAKE.size());
-    fed_trainer_log.log("Sent fed : " + JasmineGraphInstanceProtocol::HANDSHAKE, "info");
+    fed_trainer_log.log("Sent : " + JasmineGraphInstanceProtocol::HANDSHAKE, "info");
     bzero(data, FED_DATA_LENGTH);
     read(sockfd, data, FED_DATA_LENGTH);
     string response = (data);
@@ -132,23 +216,23 @@ bool JasmineGraphFederatedInstance::initiateTrain(std::string host, int port, in
     response = utils.trim_copy(response, " \f\n\r\t\v");
 
     if (response.compare(JasmineGraphInstanceProtocol::HANDSHAKE_OK) == 0) {
-        fed_trainer_log.log("Received fed : " + JasmineGraphInstanceProtocol::HANDSHAKE_OK, "info");
+        fed_trainer_log.log("Received : " + JasmineGraphInstanceProtocol::HANDSHAKE_OK, "info");
         string server_host = utils.getJasmineGraphProperty("org.jasminegraph.server.host");
         write(sockfd, server_host.c_str(), server_host.size());
-        fed_trainer_log.log("Sent fed : " + server_host, "info");
+        fed_trainer_log.log("Sent : " + server_host, "info");
 
         write(sockfd, JasmineGraphInstanceProtocol::INITIATE_FILES.c_str(),
               JasmineGraphInstanceProtocol::INITIATE_FILES.size());
-        fed_trainer_log.log("Sent fed : " + JasmineGraphInstanceProtocol::INITIATE_FILES, "info");
+        fed_trainer_log.log("Sent : " + JasmineGraphInstanceProtocol::INITIATE_FILES, "info");
         bzero(data, FED_DATA_LENGTH);
         read(sockfd, data, FED_DATA_LENGTH);
         response = (data);
         response = utils.trim_copy(response, " \f\n\r\t\v");
         std::cout << response << std::endl;
         if (response.compare(JasmineGraphInstanceProtocol::OK) == 0) {
-            fed_trainer_log.log("Received fed : " + JasmineGraphInstanceProtocol::OK, "info");
+            fed_trainer_log.log("Received : " + JasmineGraphInstanceProtocol::OK, "info");
             write(sockfd, (trainingArgs).c_str(), (trainingArgs).size());
-            fed_trainer_log.log("Sent fed: training args " + trainingArgs, "info");
+            fed_trainer_log.log("Sent : args " + trainingArgs, "info");
             bzero(data, FED_DATA_LENGTH);
             return 0;
 
@@ -164,6 +248,7 @@ bool JasmineGraphFederatedInstance::initiateTrain(std::string host, int port, in
 
 
 bool JasmineGraphFederatedInstance::initiateServer(std::string host, int port, int dataPort,std::string trainingArgs,int iteration, string partCount) {
+
     Utils utils;
     bool result = true;
     std::cout << pthread_self() << " host : " << host << " port : " << port << " DPort : " << dataPort << std::endl;
@@ -306,6 +391,84 @@ bool JasmineGraphFederatedInstance::initiateClient(std::string host, int port, i
             fed_trainer_log.log("Received fed : " + JasmineGraphInstanceProtocol::OK, "info");
             write(sockfd, (trainingArgs).c_str(), (trainingArgs).size());
             fed_trainer_log.log("Sent fed : training args " + trainingArgs, "info");
+            bzero(data, FED_DATA_LENGTH);
+            return 0;
+
+            }
+    } else {
+        fed_trainer_log.log("There was an error in the invoking training process and the response is :: " + response,
+                        "error");
+    }
+
+    close(sockfd);
+    return 0;
+}
+
+bool JasmineGraphFederatedInstance::mergeFiles(std::string host, int port, int dataPort,std::string trainingArgs,int iteration, string partCount) {
+    Utils utils;
+    bool result = true;
+    std::cout << pthread_self() << " host : " << host << " port : " << port << " DPort : " << dataPort << std::endl;
+    int sockfd;
+    char data[FED_DATA_LENGTH];
+    bool loop = false;
+    socklen_t len;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        std::cerr << "Cannot accept connection" << std::endl;
+        return 0;
+    }
+
+    if (host.find('@') != std::string::npos) {
+        host = utils.split(host, '@')[1];
+    }
+
+    server = gethostbyname(host.c_str());
+    if (server == NULL) {
+        std::cerr << "ERROR, no host named " << server << std::endl;
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *) server->h_addr,
+          (char *) &serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(port);
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "ERROR connecting" << std::endl;
+        //TODO::exit
+    }
+
+    bzero(data, FED_DATA_LENGTH);
+    write(sockfd, JasmineGraphInstanceProtocol::HANDSHAKE.c_str(), JasmineGraphInstanceProtocol::HANDSHAKE.size());
+    fed_trainer_log.log("Sent : " + JasmineGraphInstanceProtocol::HANDSHAKE, "info");
+    bzero(data, FED_DATA_LENGTH);
+    read(sockfd, data, FED_DATA_LENGTH);
+    string response = (data);
+
+    response = utils.trim_copy(response, " \f\n\r\t\v");
+
+    if (response.compare(JasmineGraphInstanceProtocol::HANDSHAKE_OK) == 0) {
+        fed_trainer_log.log("Received merge : " + JasmineGraphInstanceProtocol::HANDSHAKE_OK, "info");
+        string server_host = utils.getJasmineGraphProperty("org.jasminegraph.server.host");
+        write(sockfd, server_host.c_str(), server_host.size());
+        fed_trainer_log.log("Sent merge : " + server_host, "info");
+
+        write(sockfd, JasmineGraphInstanceProtocol::MERGE_FILES.c_str(),
+              JasmineGraphInstanceProtocol::MERGE_FILES.size());
+        fed_trainer_log.log("Sent merge : " + JasmineGraphInstanceProtocol::MERGE_FILES, "info");
+        bzero(data, FED_DATA_LENGTH);
+        read(sockfd, data, FED_DATA_LENGTH);
+        response = (data);
+        response = utils.trim_copy(response, " \f\n\r\t\v");
+        std::cout << response << std::endl;
+        if (response.compare(JasmineGraphInstanceProtocol::OK) == 0) {
+            fed_trainer_log.log("Received merge : " + JasmineGraphInstanceProtocol::OK, "info");
+            write(sockfd, (trainingArgs).c_str(), (trainingArgs).size());
+            fed_trainer_log.log("Sent merge : training args " + trainingArgs, "info");
             bzero(data, FED_DATA_LENGTH);
             return 0;
 
--- src/ml/federated/JasmineGraphFederatedInstance.h
+++ src/ml/federated/JasmineGraphFederatedInstance.h
@@ -15,9 +15,11 @@ public:
 
     void initiateFiles(std::string graphID, std::string trainingArgs);
     void initiateCommunication(std::string graphID, std::string trainingArgs, SQLiteDBInterface sqlite);
+    void initiateMerge(std::string graphID, std::string trainingArgs,SQLiteDBInterface sqlite);
     static bool initiateTrain(std::string host, int port, int dataPort,std::string trainingArgs,int iteration, string partCount);    
     static bool initiateServer(std::string host, int port, int dataPort,std::string trainingArgs,int iteration, string partCount);
     static bool initiateClient(std::string host, int port, int dataPort,std::string trainingArgs,int iteration, string partCount);
+    static bool mergeFiles(std::string host, int port, int dataPort,std::string trainingArgs,int iteration, string partCount);
     
 };
 const int FED_DATA_LENGTH = 300;
