85125d1df47f7ff1a0c6d54bd4f5b757b9127788
kasundharmadasa <kasunpdh@gmail.com>
2021-02-21 12:48:21 +0530

    sf

--- src/frontend/JasmineGraphFrontEnd.cpp
+++ src/frontend/JasmineGraphFrontEnd.cpp
@@ -860,6 +860,7 @@ void *frontendservicesesion(std::string masterIP, int connFd, SQLiteDBInterface 
 
             for (int i = 0; i < workerListSize; i++) {
 
+                string host = "localhost";
              /*   Utils::worker currentWorker = workerList.at(i);
                 string host = currentWorker.hostname;
                 string workerID = currentWorker.workerID;*/
