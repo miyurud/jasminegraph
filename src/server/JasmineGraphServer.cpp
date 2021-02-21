79f79439c402c045dbde885cf5e36d755199dc96
kasundharmadasa <kasunpdh@gmail.com>
2021-02-21 20:07:42 +0530

    d

--- src/server/JasmineGraphServer.cpp
+++ src/server/JasmineGraphServer.cpp
@@ -2569,7 +2569,7 @@ void JasmineGraphServer::pageRank() {
 
         if (sockfd < 0) {
             std::cerr << "Cannot accept connection" << std::endl;
-            return 0;
+           
         }
 
         std::string host = worker.hostname;
