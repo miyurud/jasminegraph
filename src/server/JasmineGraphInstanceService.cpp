0dd723327219042cfe2f0849383ddc7c28af810e
kasundharmadasa <kasunpdh@gmail.com>
2021-03-20 14:06:47 +0530

    a

--- src/server/JasmineGraphInstanceService.cpp
+++ src/server/JasmineGraphInstanceService.cpp
@@ -628,8 +628,8 @@ void *instanceservicesession(void *dummyPt) {
                 std::vector <string> workerSocketPair;
                 boost::split(workerSocketPair, *workerIt, boost::is_any_of(":"));
 
-                instance_logger.log("Worker ip " + workerSockerPair[0], "info");
-                instance_logger.log("Worker port " + workerSockerPair[1], "info");
+                instance_logger.log("Worker ip " + workerSocketPair[0], "info");
+                instance_logger.log("Worker port " + workerSocketPair[1], "info");
 
 
             }
