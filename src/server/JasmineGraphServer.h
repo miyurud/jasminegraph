e957bfd39d7b7c3ad2819a8f67a3c2c4d20d06ce
kasundharmadasa <kasunpdh@gmail.com>
2021-03-24 21:25:32 +0530

    a

--- src/server/JasmineGraphServer.h
+++ src/server/JasmineGraphServer.h
@@ -139,7 +139,7 @@ public:
     //return hostWorkerMap
     static std::vector<JasmineGraphServer::workers> getHostWorkerMap();
 
-    std::map<std::string, workerPartitions> getWorkerPartitions(std::string graphID);
+    std::map<std::string, workerPartition> getWorkerPartitions(std::string graphID);
 
     std::map<std::string, workerPartitions> getGraphPartitionedHosts(std::string graphID);
 
