bf81743a2b4cdb507603e022b895706611192557
chinthaka <chinthakarukshan@gmail.com>
2021-11-14 00:12:22 +0530

    fixed issue

--- src/server/JasmineGraphServer.cpp
+++ src/server/JasmineGraphServer.cpp
@@ -2063,7 +2063,7 @@ void JasmineGraphServer::addHostsToMetaDB(std::string host, std::vector<int> por
 
         if (!utils.hostExists(name, ip_address, std::to_string(workerPort), this->sqlite)) {
             string hostID = Utils::getHostID(name, this->sqlite);
-            sqlStatement = ("INSERT INTO worker (host_idhost,name,ip,user,is_public,server_port,server_data_port) VALUES(\"" +
+            sqlStatement = ("INSERT INTO worker (host_idhost,name,ip,user,is_public,server_port,server_data_port) VALUES (\"" +
                             hostID + "\", \"" + name + "\", \"" + ip_address + "\",\""+user+"\", \"\",\""+ std::to_string(workerPort) +
                             "\", \""+ std::to_string(workerDataPort) +"\")");
             this->sqlite.runInsert(sqlStatement);
