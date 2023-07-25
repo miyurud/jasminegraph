742be022402a806c0917f458edf288c377b04d79
thevindu-w <thevinduwijesekera99@gmail.com>
2023-07-25 17:40:00 +0530

    Fixed segfault when uploading graphs

--- run-docker.sh
+++ run-docker.sh
@@ -69,6 +69,7 @@ else
     fi
 fi
 
+export LD_LIBRARY_PATH=/home/ubuntu/software/metis/metis-5.1.0/build/Linux-x86_64/libmetis
 if [ $MODE -eq 1 ] ;
 then
     ./JasmineGraph "docker" $MODE $MASTERIP $WORKERS $WORKERIP $ENABLE_NMON
