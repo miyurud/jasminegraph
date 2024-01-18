c49a88a1c302a5a33a584e1fe235db76fc102188
Ashokkumar S <asokkumarsusitharan@gmail.com>
2024-01-18 21:49:08 +0530

    Correct header issues

--- src/streamingdb/StreamingSQLiteDBInterface.h
+++ src/streamingdb/StreamingSQLiteDBInterface.h
@@ -1,5 +1,5 @@
 /**
-Copyright 2018 JasminGraph Team
+Copyright 2020-2024 JasmineGraph Team
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -9,7 +9,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
- */
+**/
 
 #ifndef JASMINEGRAPH_STREAMINGSQLITEDBINTERFACE_H
 #define JASMINEGRAPH_STREAMINGSQLITEDBINTERFACE_H
