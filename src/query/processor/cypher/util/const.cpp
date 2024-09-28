0cc654110342ff607c17af5e6298e1d7f0bd2b69
thamindu <thamindu.20@cse.mrt.ac.lk>
2024-09-28 14:56:40 +0530

    query planner start

---
+++ src/query/processor/cypher/util/const.cpp
@@ -0,0 +1,5 @@
+//
+// Created by thamindu on 9/28/24.
+//
+
+#include "const.h"
---
+++ src/query/processor/cypher/util/const.h
@@ -0,0 +1,17 @@
+//
+// Created by thamindu on 9/28/24.
+//
+#include <string>
+
+#ifndef CONST_H
+#define CONST_H
+using namespace std;
+
+class Conts
+{
+public:
+    static string JASMINEGRAPH_EXECUTABLE;
+
+}
+
+#endif //CONST_H
