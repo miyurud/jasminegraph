b1263481952e00dfa850dce060aaad5244504936
Miyuru Dayarathna <miyurud@yahoo.co.uk>
2024-11-06 00:37:06 +0530

    Add docker compose and related scripts

---
+++ stop-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Variables
+HOST="localhost"
+PORT="7777"
+
+# Establish telnet connection and send command
+(
+sleep 5
+echo "shdn"
+sleep 5
+echo "exit"
+sleep 5
+) | telnet "$HOST" "$PORT"
+
+
+
+docker compose stop prometheus pushgateway &
