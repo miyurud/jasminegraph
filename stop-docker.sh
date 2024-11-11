822c18859a9832554c379a8d8d6162f7e59a047e
Miyuru Dayarathna <miyurud@yahoo.co.uk>
2024-11-11 23:11:33 +0530

    Fix formatting issue

--- stop-docker.sh
+++ stop-docker.sh
@@ -6,13 +6,11 @@ PORT="7777"
 
 # Establish telnet connection and send command
 (
-sleep 5
-echo "shdn"
-sleep 5
-echo "exit"
-sleep 5
+    sleep 5
+    echo "shdn"
+    sleep 5
+    echo "exit"
+    sleep 5
 ) | telnet "$HOST" "$PORT"
-
-
 
 docker compose stop prometheus pushgateway &
