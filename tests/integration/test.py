14bc1104a498ada25dfb1315d9ff04cd6c5c6604
ParameswaranSajeenthiran <psajeendran@gmail.com>
2025-06-07 09:59:59 +0530

    reverted graph ID in cypher test

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -239,7 +239,7 @@ def test(host, port):
         print()
         logging.info('1. Testing cypher query after adding the graph')
         send_and_expect_response(sock, 'cypher', CYPHER, b'Graph ID:', exit_on_failure=True)
-        send_and_expect_response(sock, 'cypher', b'6', b'Input query :', exit_on_failure=True)
+        send_and_expect_response(sock, 'cypher', b'1', b'Input query :', exit_on_failure=True)
         send_and_expect_response(sock, 'cypher', b'match (n) where id(n)=1 return n',
                                  b'{"n":{"id":"1","partitionID":"1"}}', exit_on_failure=True)
         send_and_expect_response(sock, 'cypher', b'',
@@ -249,7 +249,7 @@ def test(host, port):
         logging.info('2. Testing cypher aggregate query after adding the graph')
         send_and_expect_response(sock, 'cypher', CYPHER, b'Graph ID:', exit_on_failure=True)
         # send_and_expect_response(sock, 'cypher', CYPHER, b'Graph ID:', exit_on_failure=True)
-        send_and_expect_response(sock, 'cypher', b'6', b'Input query :', exit_on_failure=True)
+        send_and_expect_response(sock, 'cypher', b'1', b'Input query :', exit_on_failure=True)
         send_and_expect_response(sock, 'cypher', b'match (n) where n.id < 10 return avg(n.id)',
                                  b'{"avg(n.id)":4.5}', exit_on_failure=True)
         send_and_expect_response(sock, 'cypher', b'',
