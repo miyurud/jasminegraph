f155328f05d812e364304891e07af06bfb4cfc4c
abineyan <abineyan034@gmail.com>
2025-07-14 16:59:02 +0530

    Add integration test for Cypher ORDER BY clause

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -102,7 +102,6 @@ def test(host, port):
     """Test the JasmineGraph server by sending a series of commands and checking the responses."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((host, port))
-        #
         print()
         logging.info('Testing lst')
         send_and_expect_response(sock, 'Initial lst', LIST, EMPTY)
@@ -407,13 +406,58 @@ def test(host, port):
                                  b'done', exit_on_failure=True)
 
         print()
+        logging.info('[Cypher] OrderBy: Test 1 ')
+        send_and_expect_response(sock, 'cypher', CYPHER, b'Graph ID:', exit_on_failure=True)
+        send_and_expect_response(sock, 'cypher', b'1', b'Input query :', exit_on_failure=True)
+        send_and_expect_response(sock, 'cypher',b"match (n) where n.partitionID = '1' return n order by"
+                                                b' n.name ASC',
+                                 b'''{"n":{"category":"Studio","id":"15","label":"Location",'''
+                                 b'''"name":"Art Studio","partitionID":"1"}}''',
+                                 exit_on_failure=True)
+        send_and_expect_response(sock, 'cypher', b'',
+                                 b'{"n":{"id":"1","label":"Person","name":"Bob","occupation":"Banker",'
+                                 b'"partitionID":"1"}}', exit_on_failure=True)
+
+        send_and_expect_response(sock, 'cypher', b'',
+                                 b'{"n":{"id":"3","label":"Person","name":"David","occupation":"Doctor",'
+                                 b'"partitionID":"1"}}', exit_on_failure=True)
+
+        send_and_expect_response(sock, 'cypher', b'',b'{"n":{"id":"11","label":"Person",'
+                                                     b'"name":"George","occupation":"Chef","partitionID":"1"}}', exit_on_failure=True)
+
+        send_and_expect_response(sock, 'cypher', b'',
+                                 b'{"n":{"category":"Restaurant","id":"17","label":"Location",'
+                                 b'"name":"Gourmet Bistro","partitionID":"1"}}', exit_on_failure=True)
+
+        send_and_expect_response(sock, 'cypher', b'',
+                                 b'{"n":{"category":"School","id":"5","label":"Location",'
+                                 b'"name":"Greenfield School","partitionID":"1"}}', exit_on_failure=True)
+
+        send_and_expect_response(sock, 'cypher', b'',b'{"n":{"id":"13","label":"Person",'
+                                                     b'"name":"Ian","occupation":"Pilot","partitionID":"1"}}', exit_on_failure=True)
+
+        send_and_expect_response(sock, 'cypher', b'',
+                                 b'{"n":{"category":"Coworking Space","id":"19","label":"Location",'
+                                 b'"name":"Innovation Hub","partitionID":"1"}}', exit_on_failure=True)
+
+        send_and_expect_response(sock, 'cypher', b'',
+                                 b'{"n":{"category":"Bank","id":"7","label":"Location","name":"Town Bank",'
+                                 b'"partitionID":"1"}}', exit_on_failure=True)
+
+        send_and_expect_response(sock, 'cypher', b'',
+                                 b'{"n":{"category":"Hospital","id":"9","label":"Location",'
+                                 b'"name":"Town General Hospital","partitionID":"1"}}', exit_on_failure=True)
+        send_and_expect_response(sock, 'cypher', b'',
+                                 b'done', exit_on_failure=True)
+
+        print()
         logging.info('[Cypher] NodeScanByLabel: Test 1 ')
         send_and_expect_response(sock, 'cypher', CYPHER, b'Graph ID:', exit_on_failure=True)
         send_and_expect_response(sock, 'cypher', b'2', b'Input query :', exit_on_failure=True)
         send_and_expect_response(sock, 'cypher',b'match(n:Person) where n.id=2 return n'
                                                 b' RETURN n',
                                  b'{"n":{"id":"2","label":"Person","name":"Charlie","occupation":"IT Engineer",'
-                                     b'"partitionID":"0"}}',
+                                 b'"partitionID":"0"}}',
                                  exit_on_failure=True)
         send_and_expect_response(sock, 'cypher', b'',
                                  b'done', exit_on_failure=True)
