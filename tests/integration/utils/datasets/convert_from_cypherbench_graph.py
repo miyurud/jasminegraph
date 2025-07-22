4c5fedb4f476cd98d1a22729c44d2e97546b2276
ParameswaranSajeenthiran <psajeendran@gmail.com>
2025-07-22 11:19:18 +0530

    Fix python lint

--- tests/integration/utils/datasets/convert_from_cypherbench_graph.py
+++ tests/integration/utils/datasets/convert_from_cypherbench_graph.py
@@ -9,8 +9,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-"""Convert CypherBench-style JSON graph to JasmineGraph-compatible format."""
-
 import json
 import uuid
 
