808cdf5b666f88cacd57711f3b86ef421aa7caad
ParameswaranSajeenthiran <psajeendran@gmail.com>
2025-07-29 19:12:26 +0530

    rename path of suppression file

--- run-vtune-hostpot-analysis.sh
+++ run-vtune-hostpot-analysis.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 docker cp $(docker ps --filter "publish=7777" --format "{{.Names}}"):/home/ubuntu/software/jasminegraph/JasmineGraph .
-
+CONTAINER_ID=$(docker ps --filter "publish=7777" --format "{{.ID}}")
+PID=$(docker inspect --format '{{.State.Pid}}' "$CONTAINER_ID")
+sudo nsenter -t PID -p ps aux
