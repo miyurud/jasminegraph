4a914f44c52bcf7b169b7b23855a6e827ab2edd5
abineyan <abineyan034@gmail.com>
2025-10-11 14:40:17 +0530

    swap orderby query for large graph to the end of the test script

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -478,16 +478,7 @@ def test(host, port):
         send_and_expect_response(sock, 'cypher', b'',
                                  b'done', exit_on_failure=True)
         
-        print()
-        logging.info('[Cypher] Testing OrderBy for Large Graph')
-        send_and_expect_response(sock, 'cypher', CYPHER, b'Graph ID:', exit_on_failure=True)
-        send_and_expect_response(sock, 'cypher', b'4', b'Input query :', exit_on_failure=True)
-        send_and_expect_response_file(sock,'cypher', b'MATCH (n) RETURN n.id, n.name, n.code '
-                                                     b'ORDER BY n.code ASC',
-                                      'tests/integration/utils/expected_output/'
-                                      'orderby_expected_output_file.txt',exit_on_failure=True)
-
-
+        
         print()
         logging.info('[Cypher] Testing Undirected Relationship Type Scan')
         send_and_expect_response(sock, 'cypher', CYPHER, b'Graph ID:', exit_on_failure=True)
@@ -608,6 +599,14 @@ def test(host, port):
         send_and_expect_response(sock, 'rmgr', b'2', DONE, exit_on_failure=True)
         send_and_expect_response(sock, 'rmgr', RMGR, SEND, exit_on_failure=True)
         send_and_expect_response(sock, 'rmgr', b'3', DONE, exit_on_failure=True)
+        print()
+        logging.info('[Cypher] Testing OrderBy for Large Graph')
+        send_and_expect_response(sock, 'cypher', CYPHER, b'Graph ID:', exit_on_failure=True)
+        send_and_expect_response(sock, 'cypher', b'4', b'Input query :', exit_on_failure=True)
+        send_and_expect_response_file(sock,'cypher', b'MATCH (n) RETURN n.id, n.name, n.code '
+                                                     b'ORDER BY n.code ASC',
+                                      'tests/integration/utils/expected_output/'
+                                      'orderby_expected_output_file.txt',exit_on_failure=True)
         send_and_expect_response(sock, 'rmgr', RMGR, SEND, exit_on_failure=True)
         send_and_expect_response(sock, 'rmgr', b'4', DONE, exit_on_failure=True)
 
