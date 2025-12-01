1135ae8b5297f104dd357a23b209be1b21c328af
ParameswaranSajeenthiran <psajeendran@gmail.com>
2025-12-01 19:46:50 +0530

    Update wait comment to 1 min

--- src/frontend/JasmineGraphFrontEnd.cpp
+++ src/frontend/JasmineGraphFrontEnd.cpp
@@ -3426,7 +3426,7 @@ void JasmineGraphFrontEnd::stop_graph_streaming(int connFd, bool *loop_exit_p) {
     if (it != stopFlags.end()) {
         *(it->second) = true;
 
-        int maxWaits = 12;  // Wait up to 6 minute (12 * 5 seconds)
+        int maxWaits = 12;  // Wait up to 1 minute (12 * 5 seconds)
         int waits = 0;
         while (*(it->second) && waits < maxWaits) {
             sleep(5);
