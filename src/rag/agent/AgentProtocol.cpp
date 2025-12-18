aa7ac103c28e536a671d9db73e38f650379223ca
CSE TUF 08 G2 <abineyan034@gmail.com>
2025-12-18 17:49:22 +0530

    implement agent request struct to handle llm and queries

--- src/rag/agent/AgentProtocol.cpp
+++ src/rag/agent/AgentProtocol.cpp
@@ -7,16 +7,25 @@
 
 Logger agent_protocol_logger;
 
-std::string AgentProtocol::getPlan(const std::string& query) {
+std::string AgentProtocol::getPlan(const AgentRequestContext& agentRequesCtx) {
     
     static std::unique_ptr<Agent> agent=nullptr;
     if (!agent){
-        std::string model = "";
-        std::string host = "";
-        agent_protocol_logger.info("Initializing Agent with model=" + model + ", host=" + host);
-        agent.reset(new Agent(model, host));
+        agent_protocol_logger.info(
+            "Initializing Agent | model=" + agentRequestCtx.llm_model +
+            " | runner=" + agentRequestCtx.llm_runner +
+            " | engine=" + agentRequestCtx.llm_engine
+        );
+        //agent.reset(new Agent(model, host));
+        agent.reset(new Agent(
+            agentRequestCtx.llm_model,
+            agentRequestCtx.llm_runner   // host / backend selector
+        ));
     }
-    agent_protocol_logger.debug("Generating plan for query: " + query);
+    agent_protocol_logger.debug(
+        "Generating plan for graph=" + agentRequestCtx.graph_id +
+        " | query=" + agentRequestCtx.query
+    );
 
-    return agent->generatePlan(query);
+    return agent->generatePlan(agentRequestCtx.query);
 }
--- src/rag/agent/AgentProtocol.h
+++ src/rag/agent/AgentProtocol.h
@@ -1,7 +1,16 @@
 #pragma once
 #include <string>
 
+struct AgentRequestContext {
+    std::string query;
+    std::string graphId;
+
+    std::string llmRunner;
+    std::string llmEngine;
+    std::string llmModel;
+}
+
 class AgentProtocol {
     public:
-    static std::string getPlan(const std::string& query);
+    static std::string getPlan(const AgentRequestContext& agnetRequestCtx);
 };
