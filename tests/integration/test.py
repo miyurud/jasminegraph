eb11fe14385918f42dfe7c77be4d739ac939c261
Gayan Kaushalya <gaykaush@gmail.com>
2026-03-25 14:31:33 +0530

    Minor change

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -374,7 +374,6 @@ def test(host, port):  # pylint: disable=too-many-branches
         print()
         logging.info('2. Testing cypher aggregate query after adding the graph')
         send_and_expect_response(sock, 'cypher', CYPHER, b'Graph ID:', exit_on_failure=True)
-        # send_and_expect_response(sock, 'cypher', CYPHER, b'Graph ID:', exit_on_failure=True)
         send_and_expect_response(sock, 'cypher', b'2', b'Input query :', exit_on_failure=True)
         send_and_expect_response(sock, 'cypher', b'match (n) where n.id < 10 return avg(n.id)',
                                  b'{"avg(n.id)":4.5}', exit_on_failure=True)
