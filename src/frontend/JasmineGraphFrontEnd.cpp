77af1c8c06ce8dad02b28878350eeebedf6d4a58
Nayanatharapmc <chethmi.21@cse.mrt.ac.lk>
2026-05-02 15:23:30 +0530

    Fix formatting of write call in stop_stream_kafka_command for improved readability

--- src/frontend/JasmineGraphFrontEnd.cpp
+++ src/frontend/JasmineGraphFrontEnd.cpp
@@ -2703,7 +2703,8 @@ static void stop_stream_kafka_command(int connFd, const std::string &topicName, 
             *loop_exit_p = true;
             return;
         }
-                                                                                                                                                                                                                                                                         result_wr = write(connFd, Conts::CARRIAGE_RETURN_NEW_LINE.c_str(), Conts::CARRIAGE_RETURN_NEW_LINE.size());
+                                                                        result_wr = write(connFd, Conts::CARRIAGE_RETURN_NEW_LINE.c_str(),
+                                                                                  Conts::CARRIAGE_RETURN_NEW_LINE.size());
         if (result_wr < 0) {
             frontend_logger.error("Error writing to socket");
             *loop_exit_p = true;
